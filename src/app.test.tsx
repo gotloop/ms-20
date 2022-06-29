7570375d3e9e3692089ca66a1f28fef57d3481db
Benjamin Legrand <benjilegnard@protonmail.com>
2022-06-29 17:52:27 +0200

    test: add testing-library, vitest, a11y tests with axe and c8 for coverage

---
+++ src/app.test.tsx
@@ -0,0 +1,32 @@
+import { afterEach, describe, expect, it } from "vitest";
+import { h } from "preact";
+import { render } from "@testing-library/preact";
+import { App } from "./app";
+import axe from "axe-core";
+
+describe("app", () => {
+  afterEach(() => {
+    // this is needed to avoid doubles in the dom
+    document.body.innerHTML = "";
+  });
+  it("should render without error", () => {
+    const { container } = render(<App />, {});
+    expect(container).toBeDefined();
+  });
+  it("should be axe-essible", async () => {
+    const { container } = render(<App />);
+    const config = {
+      rules: {
+        "color-contrast": { enabled: false },
+      },
+    };
+    const results = await axe.run(container.getRootNode(), config);
+    for (const violation of results.violations) {
+      console.error(violation.description, violation.helpUrl);
+      for (const node of violation.nodes) {
+        console.error(node.failureSummary, node.html);
+      }
+    }
+    expect(results.violations).toHaveLength(0);
+  });
+});
