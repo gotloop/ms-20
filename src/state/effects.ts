a1daee4ce7a37ce23e251dc5ad8509194075cf32
Benjamin Legrand <benjilegnard@protonmail.com>
2025-03-09 22:20:23 +0100

    feat: add volume mixer knobs and related effects

--- src/state/effects.ts
+++ src/state/effects.ts
@@ -1,8 +1,15 @@
-import { computed, effect } from "@preact/signals";
+import { effect } from "@preact/signals";
 import { state } from "./state";
-import { audioContext, oscillator1Node, oscillator2Node } from "./audio-graph";
+import {
+	audioContext,
+	oscillator1Node,
+	oscillator2Node,
+	oscillator1GainNode,
+	oscillator2GainNode,
+} from "./audio-graph";
 import { oscillator1Frequency, oscillator2Frequency } from "./computed";
 
+// --- oscillators
 // link computed frequencies to oscillator nodes
 effect(() => {
 	oscillator1Node.frequency.value = oscillator1Frequency.value;
@@ -15,6 +22,18 @@ effect(() => {
 effect(() => {
 	oscillator2Node.detune.value = state.currentSetting.oscillator2.pitch.value;
 });
+
+// --- volume mixer
+effect(() => {
+	oscillator1GainNode.gain.value =
+		state.currentSetting.oscillatorsMixer.volume1.value;
+});
+effect(() => {
+	oscillator2GainNode.gain.value =
+		state.currentSetting.oscillatorsMixer.volume2.value;
+});
+
+// --- keyboard / playing state
 
 const firstTouchDone = false;
 
