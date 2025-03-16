52d2191c3e5d10a164e5aabb5935c084c85531b1
Benjamin Legrand <benjilegnard@protonmail.com>
2025-03-16 22:57:02 +0100

    feat: frequency visualizer

--- src/state/audio-graph.ts
+++ src/state/audio-graph.ts
@@ -1,12 +1,4 @@
-// TODO: separate context instanciation from module exports
-// (add a creation method)
-interface Nodes {
-	context: AudioContext;
-	osc1: {
-		oscillator: OscillatorNode;
-		gain: GainNode;
-	};
-}
+export const FFT_SIZE = 2048;
 
 export const audioContext = new AudioContext();
 
@@ -18,9 +10,21 @@ export const oscillator1GainNode = audioContext.createGain();
 
 export const oscillator2GainNode = audioContext.createGain();
 
+export const analyzerNode1 = audioContext.createAnalyser();
+analyzerNode1.fftSize = FFT_SIZE;
+
+export const analyzerBuffer1 = new Uint8Array(analyzerNode1.frequencyBinCount);
+
+export const analyzerNode2 = audioContext.createAnalyser();
+analyzerNode2.fftSize = FFT_SIZE;
+
+export const analyzerBuffer2 = new Uint8Array(analyzerNode2.frequencyBinCount);
+
 export const masterVolumeNode = audioContext.createGain();
 
 export const output = audioContext.destination;
+
+// CONNECT ALL THE THINGS
 
 oscillator1Node.connect(oscillator1GainNode);
 
@@ -29,6 +33,10 @@ oscillator2Node.connect(oscillator2GainNode);
 oscillator1GainNode.connect(masterVolumeNode);
 
 oscillator2GainNode.connect(masterVolumeNode);
+
+oscillator1GainNode.connect(analyzerNode1);
+
+oscillator2GainNode.connect(analyzerNode2);
 
 masterVolumeNode.connect(output);
 
