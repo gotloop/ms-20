105e83fc3a57809beac3ff23fdf90d254f8a8132
Benjamin Legrand <benjilegnard@protonmail.com>
2025-03-16 22:01:52 +0100

    fix: number-knox step and portamento connection to state

--- src/containers/portamento.panel.tsx
+++ src/containers/portamento.panel.tsx
@@ -1,14 +1,18 @@
 import { NumberKnob } from "../components/number-knob";
+import { state } from "../state/state";
 
-export const PortamentoPanel = () => (
-	<div class="knobs-panel">
-		<NumberKnob
-			id="portamento"
-			label="Portamento"
-			min={0}
-			max={100}
-			value={0}
-			onChange={(value) => console.log(value)}
-		></NumberKnob>
-	</div>
-);
+export const PortamentoPanel = () => {
+	const { portamento } = state.currentSetting;
+	return (
+		<div class="knobs-panel">
+			<NumberKnob
+				id="portamento"
+				label="Portamento"
+				min={0}
+				max={100}
+				value={portamento.value}
+				onChange={(value) => portamento.value = value}
+			></NumberKnob>
+		</div>
+	)
+};
